5801e998076d9c8d6d9c2921c918a2791ecf6534
Ola Lovholm <ola.lovholm@origo.oslo.kommune.no>
2024-09-25 15:18:52 +0200

    Fjerner flyttede filer, ramme for ny komponent TodoList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="bg-primary text-accent">
      It's live
+        <TodoList></TodoList>
     </div>
   );
 }
