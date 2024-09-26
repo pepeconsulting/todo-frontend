55246d3f7c262a7a335b94e72d5e84fa1449f3e9
makoalex <macovei.a2@gmail.com>
2024-09-26 14:20:01 +0200

    [fix] adding async await

--- src/clients/TodoTaskClient.js
+++ src/clients/TodoTaskClient.js
@@ -1,40 +1,52 @@
+// export const getTodoListFetch = async (url) => {
+//     return fetch(url,
+//         {
+//             headers: {
+//                 'Accept': 'application/json',
+//             },
+//             method: "GET",
+//         })
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log('Success:', data);
+//             return data
+//         })
+//         .catch((error) => {
+//             console.error('Error:', error);
+//             throw Error(error)
+//         });
+// }
 
 export const getTodoListFetch = async (url) => {
-    return fetch(url,
-        {
-            headers: {
-                'Accept': 'application/json',
-            },
-            method: "GET",
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-            return data
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-            throw Error(error)
-        });
-}
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Accept: "application/json",
+      },
+      method: "GET",
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
 
-export const postTodoListFetch = (url, data) => {
-    return fetch(url,
-        {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            method: "POST",
-            body: JSON.stringify(data)
-        })
-        .then(response => response.text())
-        .then(data => {
-            console.log('Success:', data);
-            return data
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-            throw Error(error)
-        });
-}
+export const postTodoListFetch = async (url, data) => {
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+    const responseData = await response.text();
+    return responseData;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
