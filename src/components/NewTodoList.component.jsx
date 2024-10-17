13e954349c6ce59a2d329e6af1321a8963e969eb
makoalex <macovei.a2@gmail.com>
2024-10-17 22:01:43 +0200

    [style] basic styling and layout

--- src/components/NewTodoList.component.jsx
+++ src/components/NewTodoList.component.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { postTodoListFetch } from "../clients/TodoTaskClient";
+import CompletedButtons from "./CompletedButtons";
 
 const NewTodoListComponent = ({ resetTodoList }) => {
   const [taskName, setTaskName] = useState("");
@@ -15,6 +16,7 @@ const NewTodoListComponent = ({ resetTodoList }) => {
       throw error;
     }
   };
+  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,17 +24,20 @@ const NewTodoListComponent = ({ resetTodoList }) => {
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input
+    <section className=" w-full text-left py-2 m-auto flex justify-center flex-col mb-6  border-b border-gray-500  ">
+      <form className="" onSubmit={handleSubmit}>
+        <input  className="text-sm font-light w-[270px] p-1 rounded text-accent mr-5 focus:outline-none   focus:border-y-4 focus:border-accent"  
           type="text"
-          placeholder="Enter list name"
+          required
+          placeholder="New ToDo"
           value={taskName}
           onChange={(e) => setTaskName(e.target.value)}
         />
-        <button type="submit">Add List</button>
+        
+        <button className=" px-3 bg-accent font-light border-none  hover:text-primary rounded-sm m-6 text-white text-sm border border-accent" type="submit">Add List</button>
       </form>
-    </>
+      <CompletedButtons />
+    </section>
   );
 };
 
