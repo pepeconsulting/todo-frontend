2b7669957cd19344a10b151ac313a4c5ed0c20db
makoalex <macovei.a2@gmail.com>
2024-10-19 18:44:16 +0200

    [feat] implementing Input component

--- src/components/NewTodoList.component.jsx
+++ src/components/NewTodoList.component.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { postTodoListFetch } from "../clients/TodoTaskClient";
 import CompletedButtons from "./CompletedButtons";
+import InputField from "./InputField";
 
 const NewTodoListComponent = ({ resetTodoList }) => {
   const [taskName, setTaskName] = useState("");
+
 
   const fetchTodo = async () => {
     try {
@@ -26,12 +28,22 @@ const NewTodoListComponent = ({ resetTodoList }) => {
   return (
     <section className=" w-full text-left py-2 m-auto flex justify-center flex-col mb-6  border-b border-gray-500  ">
       <form className="" onSubmit={handleSubmit}>
-        <input  className="text-sm font-light w-[270px] p-1 rounded text-accent mr-5 focus:outline-none   focus:border-y-4 focus:border-accent"  
-          type="text"
-          required
-          placeholder="New ToDo"
-          value={taskName}
-          onChange={(e) => setTaskName(e.target.value)}
+        <InputField
+        type={"text"}
+        value={taskName}
+        required={true}
+        placeholder="New Todo"
+        onChange={(e) => setTaskName(e.target.value)}
+        className="text-sm font-light w-[270px] p-1 rounded text-accent mr-5 focus:outline-none   focus:border-y-4 focus:border-accent" 
+        />
+        <InputField
+        type={"text"}
+        value={taskName}
+        required={true}
+        placeholder="Description"
+        onChange={(e) => setTaskName(e.target.value)}
+        className="text-sm font-light w-[270px] p-1 rounded text-accent mr-5 focus:outline-none   focus:border-y-4 focus:border-accent" 
+
         />
         
         <button className=" px-3 bg-accent font-light border-none  hover:text-primary rounded-sm m-6 text-white text-sm border border-accent" type="submit">Add List</button>
