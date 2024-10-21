ad9777d2cacf12cac16c81ed79e2daa714a17472
makoalex <macovei.a2@gmail.com>
2024-10-21 15:42:33 +0200

    [feat] adding (temporary) local storage for description field

--- src/components/NewTodoList.component.jsx
+++ src/components/NewTodoList.component.jsx
@@ -5,12 +5,18 @@ import InputField from "./InputField";
 
 const NewTodoListComponent = ({ resetTodoList }) => {
   const [taskName, setTaskName] = useState("");
-
+  const [taskDescription, setTaskDescription] = useState("");
 
   const fetchTodo = async () => {
     try {
+      if (taskName) {
+        const todoItem = { name: taskName, description: taskDescription };
+        localStorage.setItem(taskName, JSON.stringify(todoItem));
+        console.log("this is local storage", localStorage);
+      }
       await postTodoListFetch({ name: taskName });
       setTaskName("");
+      setTaskDescription("");
 
       if (resetTodoList) resetTodoList();
     } catch (error) {
@@ -18,7 +24,6 @@ const NewTodoListComponent = ({ resetTodoList }) => {
       throw error;
     }
   };
-  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -26,29 +31,32 @@ const NewTodoListComponent = ({ resetTodoList }) => {
   };
 
   return (
-    <section className=" w-full text-left py-2 m-auto flex justify-center flex-col mb-6  border-b border-gray-500  ">
+    <section className=" w-full text-left py-2 m-auto flex justify-between flex-col mb-6 border-b border-gray-500  ">
       <form className="" onSubmit={handleSubmit}>
         <InputField
-        type={"text"}
-        value={taskName}
-        required={true}
-        placeholder="New Todo"
-        onChange={(e) => setTaskName(e.target.value)}
-        className="text-sm font-light w-[270px] p-1 rounded text-accent mr-5 focus:outline-none   focus:border-y-4 focus:border-accent" 
+          type={"text"}
+          value={taskName}
+          required={true}
+          placeholder="New Todo"
+          onChange={(e) => setTaskName(e.target.value)}
+          className="text-reg font-light w-[270px] capitalize p-1 rounded text-accent mr-5 focus:outline-none focus:border-y-4 focus:border-accent"
         />
         <InputField
-        type={"text"}
-        value={taskName}
-        required={true}
-        placeholder="Description"
-        onChange={(e) => setTaskName(e.target.value)}
-        className="text-sm font-light w-[270px] p-1 rounded text-accent mr-5 focus:outline-none   focus:border-y-4 focus:border-accent" 
-
+          type={"text"}
+          value={taskDescription}
+          required={false}
+          placeholder="Description"
+          onChange={(e) => setTaskDescription(e.target.value)}
+          className="text-reg font-light w-[270px] p-1 rounded capitalize text-accent mr-5 focus:outline-none focus:border-y-4 focus:border-accent"
         />
-        
-        <button className=" px-3 bg-accent font-light border-none  hover:text-primary rounded-sm m-6 text-white text-sm border border-accent" type="submit">Add List</button>
+
+        <button
+          className=" px-5 bg-accent font-light border-none btn-sm hover:text-primary rounded-sm m-6 text-white text-sm border border-accent"
+          type="submit"
+        >
+          Add{" "}
+        </button>
       </form>
-      <CompletedButtons />
     </section>
   );
 };
--- src/components/Todo.component.jsx
+++ src/components/Todo.component.jsx
@@ -5,16 +5,21 @@ import {BsCheckLg} from 'react-icons/bs';
 
 
 function TodoComponent({ todolist, resetTodoList }) {
+  const localStore = localStorage.getItem(todolist.name);
+  const description = localStore ? JSON.parse(localStore).description: '';
+  console.log('this is description', description)
+
+  
   const handleDeleteClick = () => {
     console.log("Delete button clicked, todo ID:", todolist.id); 
     deleteTodoList(`${todolist.id}`).then(setTimeout(resetTodoList, 100));
   };
 
   return (
-    <div className="w-full font-primary mb-3 bg-secondary shadow-md font-normal rounded-sm flex flex-row justify-between items-center gap-3 items-center px-5">
-      <div className="flex flex-col mr-4 items-center">
-        <h1 className="font-light text-md capitalize text-accent">{todolist.name}</h1>
-        <h3 className="font-light text-sm">Description</h3>
+    <div className="w-3/4 h-20 mb-3 bg-secondary shadow-md font-normal rounded-sm flex flex-row justify-between gap-3 items-center px-5">
+      <div className="flex flex-col mr-4 items-start">
+        <h1 className="font-extralight  text-md capitalize text-accent">{todolist.name}</h1>
+        <h3 className="font-medium text-sm capitalize italic">{description}</h3>
       </div>
       <div className="flex flex-row justify-center items-center gap-3">
       <AiOutlineDelete  
